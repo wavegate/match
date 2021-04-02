4db57afbb05b5062b152ee055d39f63b13da5b2e
wavegate <cc.frankee@gmail.com>
2021-04-02 00:39:57 -0500

    Restructure

--- mail.py
+++
@@ -1,5 +0,0 @@
-from flask_mail import Message
-msg = Message('test subject', sender=app.config['ADMINS'][0], recipients=['match0912@gmail.com'])
-msg.body = 'text body'
-msg.html = '<h1>HTML body</h1>'
-mail.send(msg)
