ab718e67d1fed5a8d50bfe9c8255eb27daa55bf7
wavegate <cc.frankee@gmail.com>
2021-04-02 17:51:02 -0500

    Program

--- app/models.py
+++ app/models.py
@@ -137,3 +137,14 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class Program(SearchableMixin, db.Model):
+    __searchable__ = ['body']
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
+
+    def __repr__(self):
+        return '<Program {}>'.format(self.body)
