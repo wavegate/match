f483f28b3d9d8a4cea4208a2f882d55a86e645f5
wavegate <cc.frankee@gmail.com>
2021-04-07 22:55:51 -0500

    remove search

--- app/main/routes.py
+++ app/main/routes.py
@@ -20,13 +20,13 @@ def index():
 	else:
 		return redirect(url_for('main.programs'))
 
-@bp.before_app_request
-def before_request():
-	if current_user.is_authenticated:
-		current_user.last_seen = datetime.utcnow()
-		db.session.commit()
-		g.search_form = SearchForm()
-	g.locale = str(get_locale())
+#@bp.before_app_request
+#def before_request():
+#	if current_user.is_authenticated:
+#		current_user.last_seen = datetime.utcnow()
+#		db.session.commit()
+#		g.search_form = SearchForm()
+#	g.locale = str(get_locale())
 
 @bp.route('/programs', methods=['GET', 'POST'])
 def programs():
@@ -37,7 +37,7 @@ def programs():
 		db.session.commit()
 		flash(_('Program added!'))
 		return redirect(url_for('main.programs'))
-	programs = Program.query.order_by(Program.timestamp.desc())
+	programs = Program.query.order_by(Program.timestamp.desc()).limit(100)
 	return render_template('programs.html', title=_('Programs'),
 						   programs=programs, form=form)
 
@@ -249,20 +249,20 @@ def translate_text():
 									  request.form['dest_language'])})
 
 
-@bp.route('/search')
-@login_required
-def search():
-	if not g.search_form.validate():
-		return redirect(url_for('main.explore'))
-	page = request.args.get('page', 1, type=int)
-	posts, total = Post.search(g.search_form.q.data, page,
-							   current_app.config['POSTS_PER_PAGE'])
-	next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
-		if total > page * current_app.config['POSTS_PER_PAGE'] else None
-	prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
-		if page > 1 else None
-	return render_template('search.html', title=_('Search'), posts=posts,
-						   next_url=next_url, prev_url=prev_url)
+#@bp.route('/search')
+#@login_required
+#def search():
+#	if not g.search_form.validate():
+#		return redirect(url_for('main.explore'))
+#	page = request.args.get('page', 1, type=int)
+#	posts, total = Post.search(g.search_form.q.data, page,
+#							   current_app.config['POSTS_PER_PAGE'])
+#	next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+#		if total > page * current_app.config['POSTS_PER_PAGE'] else None
+#	prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
+#		if page > 1 else None
+#	return render_template('search.html', title=_('Search'), posts=posts,
+#						   next_url=next_url, prev_url=prev_url)
 
 @bp.route('/user/<username>/popup')
 @login_required
--- app/models.py
+++ app/models.py
@@ -7,50 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import json
 from app import db, login
-from app.search import add_to_index, remove_from_index, query_index
-
-
-class SearchableMixin(object):
-    @classmethod
-    def search(cls, expression, page, per_page):
-        ids, total = query_index(cls.__tablename__, expression, page, per_page)
-        if total == 0:
-            return cls.query.filter_by(id=0), 0
-        when = []
-        for i in range(len(ids)):
-            when.append((ids[i], i))
-        return cls.query.filter(cls.id.in_(ids)).order_by(
-            db.case(when, value=cls.id)), total
-
-    @classmethod
-    def before_commit(cls, session):
-        session._changes = {
-            'add': list(session.new),
-            'update': list(session.dirty),
-            'delete': list(session.deleted)
-        }
-
-    @classmethod
-    def after_commit(cls, session):
-        for obj in session._changes['add']:
-            if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__, obj)
-        for obj in session._changes['update']:
-            if isinstance(obj, SearchableMixin):
-                add_to_index(obj.__tablename__, obj)
-        for obj in session._changes['delete']:
-            if isinstance(obj, SearchableMixin):
-                remove_from_index(obj.__tablename__, obj)
-        session._changes = None
-
-    @classmethod
-    def reindex(cls):
-        for obj in cls.query:
-            add_to_index(cls.__tablename__, obj)
-
-
-db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
-db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
+#from app.search import add_to_index, remove_from_index, query_index
 
 
 followers = db.Table(
@@ -180,8 +137,7 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-class Post(SearchableMixin, db.Model):
-    __searchable__ = ['body']
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -192,9 +148,8 @@ class Post(SearchableMixin, db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-class Program(SearchableMixin, db.Model):
+class Program(db.Model):
     __tablename__ = 'program'
-    __searchable__ = ['name']
     id = db.Column(db.Integer, primary_key=True)
     specialty = db.Column(db.String(140))
     name = db.Column(db.String(140))
