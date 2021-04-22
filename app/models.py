cff4148d499bc92681e38649c1f6554e3d0c79c7
wavegate <cc.frankee@gmail.com>
2021-04-22 15:08:16 -0500

    config change

--- app/models.py
+++ app/models.py
@@ -141,7 +141,7 @@ def load_user(id):
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	body = db.Column(db.String(140))
+	body = db.Column(db.String(600))
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	program_id = db.Column(db.Integer, db.ForeignKey('program.id'))
